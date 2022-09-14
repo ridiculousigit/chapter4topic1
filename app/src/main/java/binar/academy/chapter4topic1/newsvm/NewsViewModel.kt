eeb94f73d66740db1915710fb733e84b97119ed2
ridiculousigit <jsigit243@gmail.com>
2022-09-14 18:01:07 +0700

    Add some assets

--- app/src/main/java/binar/academy/chapter4topic1/newsvm/NewsViewModel.kt
+++ app/src/main/java/binar/academy/chapter4topic1/newsvm/NewsViewModel.kt
@@ -12,40 +12,49 @@ class NewsViewModel : ViewModel() {
             R.drawable.bg_news_onepiece, R.string.body_news1
         ),
         DataNews(
-            "Film One Piece: Red Raup Untung Rp 1,2 Triliun",
-            "Tia Agnes Astuti", "Jumat, 02 Sep 2022 10:51 WIB",
-            R.drawable.bg_news_onepiece,
-            R.string.body_news1
+            "Walkot Cilegon Datangi Kemenag Usai Heboh Penolakan Gereja",
+            "Anggi Muliawati", "Rabu, 14 Sep 2022 10:24 WIB",
+            R.drawable.bg_news_cilegon, R.string.body_news2
         ),
         DataNews(
-            "Film One Piece: Red Raup Untung Rp 1,2 Triliun",
-            "Tia Agnes Astuti", "Jumat, 02 Sep 2022 10:51 WIB",
-            R.drawable.bg_news_onepiece,
-            R.string.body_news1
+            "Daftar Terbaru Hukuman Etik ke 9 Polisi di Kasus Ferdi Sambo",
+            "Dwi Andayani", "Rabu, 14 Sep 2022 09:21 WIB",
+            R.drawable.bg_news_sambo, R.string.body_news3
         ),
         DataNews(
-            "Film One Piece: Red Raup Untung Rp 1,2 Triliun",
-            "Tia Agnes Astuti", "Jumat, 02 Sep 2022 10:51 WIB",
-            R.drawable.bg_news_onepiece,
-            R.string.body_news1
+            "Suciwati Buka Suara Usai Hacker Bjorka Ungkap Pembunuh Munir",
+            "Dwi Andayani", "Rabu, 14 Sep 2022 09:21 WIB<",
+            R.drawable.bg_news_munir, R.string.body_news4
         ),
         DataNews(
-            "Film One Piece: Red Raup Untung Rp 1,2 Triliun",
-            "Tia Agnes Astuti", "Jumat, 02 Sep 2022 10:51 WIB",
-            R.drawable.bg_news_onepiece,
-            R.string.body_news1),
-        DataNews(
-            "Film One Piece: Red Raup Untung Rp 1,2 Triliun",
-            "Tia Agnes Astuti", "Jumat, 02 Sep 2022 10:51 WIB",
-            R.drawable.bg_news_onepiece,
-            R.string.body_news1
+            "Penembakan Misterius 1980an Ribuan Korban Jiwa, Petrus Beraksi Pertama di Yogya",
+            "S. Dian Andryanto", "Minggu, 28 Nov 2021 15:40 WIB",
+            R.drawable.bg_news_petrus, R.string.body_news5
         ),
         DataNews(
-            "Film One Piece: Red Raup Untung Rp 1,2 Triliun",
-            "Tia Agnes Astuti",
-            "Jumat, 02 Sep 2022 10:51 WIB",
-            R.drawable.bg_news_onepiece,
-            R.string.body_news1
+            "Kronologi Kerusuhan Mei 1998",
+            "Verelladevanka Adryamarthanino", "Senin, 15 Nov 2021 15:00 WIB",
+            R.drawable.bg_news_mei, R.string.body_news6
+        ),
+        DataNews(
+            "Peristiwa G30S PKI Bukan Pelanggaran HAM Berat, tapi Makar",
+            "Tim Liputan Sindo", "Selasa, 26 Jul 2022 09:17 WIB",
+            R.drawable.bg_news_onepiece, R.string.body_news7
+        ),
+        DataNews(
+            "Eliza Mengakui Perintah Pengeboman dari Tommy Soeharto",
+            "Tim Liputan 6 SCTV", "Minggu, 21 Jan 2001, 08:26 WIB",
+            R.drawable.bg_news_bom, R.string.body_news8
+        ),
+        DataNews(
+            "Kelangsungan Pemerintahan Gus Dur Ditentukan 2001",
+            "Esther Mulyanie dan Prihandoyo", "Sabtu, 9 Des 2000, 20:52 WIB",
+            R.drawable.bg_news_gusdur, R.string.body_news9
+        ),
+        DataNews(
+            "Habibie: Tak Ada Rekayasa buat Timtim",
+            "Olivia Rosalia, Agung Nugraha dan Bambang Triono", "Jumat, 21 Mar 2003, 08:44 WIB",
+            R.drawable.bg_news_habibie, R.string.body_news10
         )
     )
 
